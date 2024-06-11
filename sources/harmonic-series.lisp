342a6dfe00d4badc0cfc45eb4886b982f166089b
Paulo Henrique Raposo <76979664+PHRaposo@users.noreply.github.com>
2024-06-11 09:38:23 -0300

    Update harmonic-series.lisp

--- sources/harmonic-series.lisp
+++ sources/harmonic-series.lisp
@@ -31,6 +31,7 @@ approx))
     :doc "Returns a harmonic series from a <fundamental>, <partials> and <approximation> (2 for semitones)."
 (harmonic-s fundamental partials approx))
 
+#|
 (defun possible-harmonic (chord-midics)
  (let* ((sub-harmonic 
          (mapcar #'(lambda (input)
@@ -61,20 +62,40 @@ approx))
   (remove-duplicates 
    (om::x-append (first results)
                      (om::x-intersect results chord))))
+|#
 
-(om::defmethod! WTVF ((chord list) (selection symbol) &optional (n 12) (mc-min -3626) (mc-max 10800))
-	:initvals '( (5100 5500 5700 5800 6100 6400 6500 6800 7100 7200 7400 7800) :highest 12 -3626 10800)
-	:indoc '("chord-midics" "highest or all" "n-common-tones" "min-midicents" "max-midicents") 
+(om::defmethod! WTVF ((chord list) (selection symbol) (mc-min integer) (mc-max integer) (n integer)) ;NEW (10/06/2024)
+	:initvals '( (5100 5500 5700 5800 6100 6400 6500 6800 7100 7200 7400 7800) :highest 1200 12000 12)
+	:indoc '("chord-midics" "highest or all" "min-midicents" "max-midicents" "n-common-tones" ) 
 	:icon 01
-        :menuins '( (1 (("highest" :highest) ("all" :all) )))
-	:numouts 2
+    :menuins '( (1 (("highest" :highest) ("all" :all) )))
+	:numouts 4
     :doc "Well Tempered Virtual Fundamental: calculates all harmonic twelve tone chords thats shares 'n' or higher common tones with the given chord.
-	The first output is a list harmonic twelve-tone chords.
-	The second output returns the same results with all fundamentals and common notes."
+	The first output returns the fundamental with common tones.
+	The second output returns the complete harmonic series, up to partial 40.
+	The third output returns the fundamental with common tones with odd partials (harmonic tweleve tone chord).
+	The fourth output returns the complete harmonic series with odd partials."
+	(let ((res (screamer::well-tempered-virt-fun chord mc-min mc-max n)))
+	 (cond ((not (null res))
+	        (if (equal selection :all)
+	            (values res
+				       (loop for r in res
+						     collect (remove-duplicates(om::approx-m (om::f->mc (om::om* (om::mc->f (car r)) (om::arithm-ser 1 40 1))) 2)))
+	  				   (loop for r in res
+	  						 collect (intersection (harmonic-ttch (car r)) chord))
+	  	  			   (loop for r in res
+	  	  					 collect (harmonic-ttch (car r))))								  	 							 
+		        (values (car res)
+			           (remove-duplicates (om::approx-m (om::f->mc (om::om* (om::mc->f (car (car res))) (om::arithm-ser 1 40 1))) 2))
+					   (reverse (intersection (harmonic-ttch (car (car res))) chord))
+					   (harmonic-ttch (car (car res))))))
+		  (t (om::om-message-dialog "UNABLE TO FIND A FUNDAMENTAL.") (om::om-abort)))))
+		 
+#|	
  (let* ((results (if (= n 12) (wtvf-recursive chord n mc-min mc-max) (well-temp-vf chord n mc-min mc-max)))
           (highest (if (not (null results)) (highest-fund results) nil)))
 
-  (cond ((null results) (write "ERROR: Unable to find a solution."))
+  (cond ((null results) (progn (om::om-message-dialog "ERROR: Unable to find a fundamental.") (om::om-abort)))
             ((and (equal selection :all) (> (length results) 1)) (values results (mapcar #'(lambda (input1) (fund-common chord input1)) results)))
             ((and (equal selection :highest)  (> (length results) 1)) (values highest (fund-common chord highest)))
             (t (if (equal selection :all) 
@@ -100,3 +121,5 @@ approx))
 (defun highest-fund (results)
  (let ((fundamentals (mapcar #'first results)))
   (nth (position (om::list-max fundamentals) fundamentals) results)))
+|#
+
